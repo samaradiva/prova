efb424ffa47a4bd34a44743d9b27a323a69e482d
geannn1612 <geanlucas13572500@gmail.com>
2024-11-29 16:35:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<h2> nome
+       <?PhP
+         ECHO "RECEITA";
+         ?>
+         </H2>
+         <TABLE border="1">
+             <THEAD>
+                <TR>
+                <TH>INGREDIENTES</TH>
+                <TH>QUANTIDADE</TH>
+</TR>
+</THEAD>
+<tbody>
+    <tr>
+        <td>batata</td>
+        <td> 1kg </td>
+</tr>
+<tr>
+    <td>margarina</td>
+   <td> 2 colheres</td>
+</tr>
+<tr>
+    <td> dente de alho</td>
+    <td> 1</td>
+</tr>
+<tr>
+    <td>leite</td>
+    <td>1/2 xícara</td>
+</tr>
+<tr>
+    <td> sal a gosto</td>
+</tr>
+</tbody>
+</table>
+<h1> nota: </h1>
+<script>
+    var h1= document.querySelector("h1");
+    h1.innerHTML="nota:10";
+    </script>
+
+
+
+
